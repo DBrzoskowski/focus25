1db3d933ab6ec8b7d969010ede6bbb2ec34615be
Damian Brzoskowski <s18499@pjwstk.edu.pl>
2021-01-17 19:50:12 +0100

    Create GoogleAuth app and service

---
+++ frontend/src/components/GoogleAuth/GoogleAuth.stories.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import GoogleAuth from './GoogleAuth';
+
+storiesOf('GoogleAuth', module).add('default', () => <GoogleAuth />);
