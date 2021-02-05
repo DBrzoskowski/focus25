a31a0c1bc6730492a15aafa98aadd013ba58d808
Damian Brzoskowski <s18499@pjwstk.edu.pl>
2021-02-05 20:27:09 +0100

    [TASK-4] - Create pomodoro and task timer

--- frontend/src/components/GoogleAuth/GoogleAuth.stories.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import GoogleAuth from './GoogleAuth';
-
-storiesOf('GoogleAuth', module).add('default', () => <GoogleAuth />);
